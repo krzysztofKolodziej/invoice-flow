c5e39d9717a168dd1ea3e4d099e965126ea2f5c4
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2026-01-16 17:17:47 +0100

    Init commit

---
+++ src/main/java/com/example/invoice_flow/InvoiceFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoice_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceFlowApplication.class, args);
+	}
+
+}
