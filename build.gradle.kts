c6b088f765a8e72f70b17e1e6f1db377b4554e38
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2026-01-17 12:23:58 +0100

    chore: create Gradle multi-module structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,9 @@
-plugins {
-	java
-	id("org.springframework.boot") version "4.0.1"
-	id("io.spring.dependency-management") version "1.1.7"
-}
+plugins {}
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
 description = "An application for processing millions of invoices using Apache Kafka"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
-	}
-}
-
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-kafka")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-kafka-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
